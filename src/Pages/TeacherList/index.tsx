d67c2ce42ab403724f0f6385391866c4a182e775
Rogério Feliciano <felicianoi9@gmail.com>
2020-08-11 12:01:35 -0300

    Top-bar Layout in  basic TeacherLIst  is done.

--- src/Pages/TeacherList/index.tsx
+++ src/Pages/TeacherList/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import "./styles.css";
+import { Link } from "react-router-dom";
+import backIcon from "../../Assets/images/icons/back.svg";
+import logoImg from "../../Assets/images/logo.svg";
 
 function TeacherList() {
     return (
-        <h1>Teachers List</h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/" > 
+                        <img src={backIcon} alt=""/>
+                    </Link> 
+                    <img src={logoImg} alt=""/>
+                </div>
+                <div className="header-content">
+                    <strong>Estes são os proffys disponíveis</strong>
+                </div>
+            </header>
+        </div>
     );
 }
 
