e39819c6b151df8893a97e603ce783bf27107262
Rogério Feliciano <felicianoi9@gmail.com>
2020-08-11 12:49:06 -0300

    Components are inited of basic default.

--- src/Pages/TeacherForm/index.tsx
+++ src/Pages/TeacherForm/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import PageHeader from "../../Components/PageHeader";
+
+import "./styles.css";
 
 function TeacherForm() {
     return (
-        <h1>Teachers form</h1>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrível que você quer daŕ aulas." />
+        </div>
     );
 }
 
--- src/Pages/TeacherList/index.tsx
+++ src/Pages/TeacherList/index.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import "./styles.css";
-import { Link } from "react-router-dom";
-import backIcon from "../../Assets/images/icons/back.svg";
-import logoImg from "../../Assets/images/logo.svg";
+import PageHeader from "../../Components/PageHeader";
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/" > 
-                        <img src={backIcon} alt=""/>
-                    </Link> 
-                    <img src={logoImg} alt=""/>
-                </div>
-                <div className="header-content">
-                    <strong>Estes são os proffys disponíveis</strong>
-                </div>
-            </header>
+            <PageHeader title="Estes são os proffys disponíveis" />
         </div>
     );
 }
